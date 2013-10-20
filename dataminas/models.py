2a2c720a242a041e668071c7d89a7265920f37ba
João Paulo Pesce <jpesce@gmail.com>
2013-10-20 10:06:22 -0200

    Database reset

--- dataminas/models.py
+++ dataminas/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class Anomaly(models.Model):
-    date = models.DateField()
-    data_type = models.CharField(max_length=255)
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    name_url = models.CharField(max_length=255)
+    parent = models.ForeignKey('self')
+
+class Point(models.Model):
+    year = models.IntegerField()
+    month = models.IntegerField()
+    value = models.IntegerField()
+    category = models.ForeignKey('Category')
     user_score = models.IntegerField()
-    algorithm_score = models.FloatField()
+    algorithm_score = models.IntegerField()
