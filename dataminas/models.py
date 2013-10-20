fd5683b572a917269bde87080db33d6850e39545
João Paulo Pesce <jpesce@gmail.com>
2013-10-20 11:08:27 -0200

    Database and models reset

--- dataminas/models.py
+++ dataminas/models.py
@@ -5,12 +5,12 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
     name_url = models.CharField(max_length=255)
-    parent = models.ForeignKey('self')
+    parent = models.ForeignKey('Category', blank=True, null=True, default=None)
 
 class Point(models.Model):
     year = models.IntegerField()
     month = models.IntegerField()
-    value = models.IntegerField()
+    value = models.DecimalField(max_digits=32, decimal_places=2)
     category = models.ForeignKey('Category')
     user_score = models.IntegerField()
     algorithm_score = models.IntegerField()
